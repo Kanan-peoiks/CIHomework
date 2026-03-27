fadf926427de6d2c2468c7f5a503b853ec1d5071
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-03-28 02:17:32 +0400

    Initial commit

---
+++ src/main/java/com/example/cihomework/CiHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.cihomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cihomework/CiHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cihomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
